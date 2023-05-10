4eb53f17ac9d47b0083d1d7452b7f6c0c17be9bf
Nazar <nazariximail@gmail.com>
2023-05-10 14:31:25 +0300

    New code

--- NEW/FileName.cpp
+++ NEW/FileName.cpp
@@ -2,9 +2,31 @@
 
 using namespace std;
 
+void IntArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter array [" << i << "] ", cin >> arr[i];
+	}
+}
+
+void PrintArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << ", ";
+	}
+}
+
 int main()
 {
-	cout << "NEW repository!";
+	int size; 
+	cout << "Enter size: ", cin >> size;
+
+	int* arr = new int[size];
+	cout << endl;
+	IntArray(arr, size);
+	PrintArray(arr, size);
 	
 	return 0;
 
