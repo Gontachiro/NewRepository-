e3af39b519a22c346a1c2f99781ef73439cafdfa
Nazar <nazariximail@gmail.com>
2023-05-10 22:10:12 +0300

    New Function Delete

--- NEW/FileName.cpp
+++ NEW/FileName.cpp
@@ -2,32 +2,52 @@
 
 using namespace std;
 
-void IntArray(int* arr, int size)
+#define SIZE 10
+
+void IntArray(int* arr)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		cout << "Enter array [" << i << "] ", cin >> arr[i];
 	}
 }
 
-void PrintArray(int* arr, int size)
+void PrintArray(int* arr)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << ", ";
 	}
 }
 
+void Zamina(int* arr, int* new_arr)
+{
+	int index = arr[5];
+	int j = 0;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] != index)
+		{
+			new_arr[j] = arr[i];
+			j++;
+		}
+	}
+}
+
 int main()
 {
-	int size; 
-	cout << "Enter size: ", cin >> size;
-
-	int* arr = new int[size];
+	int* arr = new int[SIZE];
+	int* new_arr = new int[SIZE - 1];
+	IntArray(arr);
+	PrintArray(arr);
 	cout << endl;
-	IntArray(arr, size);
-	PrintArray(arr, size);
-	
+	Zamina(arr, new_arr);
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		cout << new_arr[i] << ", ";
+	}
+
 	return 0;
 
 }
